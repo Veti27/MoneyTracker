dac62678e7067357c513453018b6b62f0123d20d
Veton Staraveci <v.staraveci@austrian-standards.at>
2020-12-16 19:01:42 +0100

    add attributes and constructor

--- src/at/htlwienwest/Person.java
+++ src/at/htlwienwest/Person.java
@@ -2,4 +2,12 @@ package at.htlwienwest;
 
 public class Person
 {
+    private int id;
+    private int age;
+
+    public Person(int id, int age)
+    {
+        this.id = id;
+        this.age = age;
+    }
 }
